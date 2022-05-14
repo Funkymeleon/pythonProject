a3c2a7018f2da9da6fee31b6704e8c909735e9e1
Funkymeleon <thoschie@gmx.de>
2022-05-14 22:23:31 +0200

    date added

--- ExtendDiscogs.py
+++ ExtendDiscogs.py
@@ -1,8 +1,8 @@
 import discogs_client
-import csv
 import pandas as pd
 import numpy as np
 
+# noinspection SpellCheckingInspection
 d = discogs_client.Client('HR3superscraper/1.0', user_token='gBtrxcMPRJXQLzXiAkeDaLlLFusTmKWgAkcUSjRV')
 #
 # results = d.search('sia david guetta', type="Release")
@@ -14,17 +14,17 @@ d = discogs_client.Client('HR3superscraper/1.0', user_token='gBtrxcMPRJXQLzXiAke
 dfsonglist = pd.read_csv('./new_list.csv')
 
 for index, row in dfsonglist.iterrows():
-    results = d.search(row['artist']+ ' ' + row['title'], type="Release")
+    results = d.search(row['artist'] + ' ' + row['title'], type="Release")
     try:
         page1 = results.page(1)
 
-        if len(page1)>0:
-            print (row['artist']+ ' ' + row['title']+' '+page1[0].data["genre"][0]+' '+page1[0].data["year"])
+        if len(page1) > 0:
+            print(row['artist'] + ' ' + row['title'] + ' ' + page1[0].data["genre"][0] + ' ' + page1[0].data["year"])
             dfsonglist.at[index, 'genre'] = page1[0].data["genre"][0]
             dfsonglist.at[index, 'year'] = int(page1[0].data["year"])
         else:
-            print ("No result: " + row['artist']+ ' ' + row['title'])
-            dfsonglist.at[index, 'genre']= np.nan
+            print("No result: " + row['artist'] + ' ' + row['title'])
+            dfsonglist.at[index, 'genre'] = np.nan
             dfsonglist.at[index, 'year'] = np.nan
         if index % 100 == 0:
             dfsonglist.to_csv('new_list.csv', index=False)
--- combine_lists.py
+++ combine_lists.py
@@ -5,7 +5,6 @@ import pandas as pd
 import glob
 import re
 
-
 mylist = [f for f in glob.glob(".\\csv\\*.csv")]
 print(mylist)
 
@@ -21,19 +20,18 @@ print()
 
 dfsonglist = dfsonglist_all.drop_duplicates(subset=['date', 'time', 'artist', 'title'], keep='first')
 
+# dfsonglist = pd.read_csv('../../pythonProject/full_list.csv', usecols=['date','time','artist','title'])
+dfsonglist['artist_stripped'] = [(re.sub('\W+', '', string).lower()) for string in dfsonglist['artist']]
+dfsonglist['title_stripped'] = [(re.sub('\W+', '', string).lower()) for string in dfsonglist['title']]
+dfsonglist = dfsonglist.sort_values(by=['date', 'time'], ignore_index=True)
+# dfsonglist
 
-#dfsonglist = pd.read_csv('../../pythonProject/full_list.csv', usecols=['date','time','artist','title'])
-dfsonglist['artist_stripped'] = [(re.sub('\W+','', string).lower()) for string in dfsonglist['artist']]
-dfsonglist['title_stripped'] = [(re.sub('\W+','', string).lower()) for string in dfsonglist['title']]
-dfsonglist = dfsonglist.sort_values(by = ['date','time'], ignore_index = True)
-#dfsonglist
+# Add year to dataframe
+dfsonglist['year'] = pd.to_datetime(dfsonglist['date']).dt.year
 
-#Add year to dataframe
-dfsonglist['year']=pd.to_datetime(dfsonglist['date']).dt.year
-
-#Add weekday to dataframe
-dfsonglist['weekday']=[x.weekday() for x in pd.to_datetime(dfsonglist['date'])]
-#dfsonglist
+# Add weekday to dataframe
+dfsonglist['weekday'] = [x.weekday() for x in pd.to_datetime(dfsonglist['date'])]
+# dfsonglist
 
 
 dfsonglist.to_csv('full_list.csv', index=False)
